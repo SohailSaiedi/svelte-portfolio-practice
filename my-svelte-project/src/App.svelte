da06e19a209cd7bae6957dc9c89911e44fc0511b
Sohail <trainee.sohail.saiedi@sigmalabs.zyx>
2021-11-01 09:57:00 +0000

    start

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+</script>
+
+<section class="header-section">
+	<div class="header-wrapper wrapper">
+		<div class="header-left-wrapper">
+			<h1 class="header-title text-white">Sohail Saiedi</h1>
+		</div>
+		<div class="header-right-wrapper">
+			<nav class="nav-bar">
+				<a href="/services">Services</a>
+				<a href="/works">Works</a>
+				<a href="/blogs">Blogs</a>
+			</nav> 
+			<div class="intro-wrapper">
+				<h6>-- Introduction</h6>
+				<h2>Product Designer and Developer, based in California</h2>
+			</div>
+			<div class="story-wrapper">
+				<h3>My Story   &#8594;</h3>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+	.header-title{
+		width: 600px;
+		font-size: 6rem;
+	}
+	.header-section{
+		background-color: #3d4053;
+		height: 100vh;
+		background-image: url('https://assets.grooveapps.com/images/5fff4a731d2cfa006f386042/1624073574_Professional-Headshots-Portraits-by-Jared-Wolfe-Alexandria-VA-Portrait-Studio-Testimonial-03.png');
+		background-repeat: no-repeat;
+		background-position: center bottom;
+		background-size: 50%;
+
+	}
+	.wrapper{
+		width: 900px;
+		height: 100%;
+		margin: 0 auto;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.header-wrapper{
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: bolder;
+	}
+
+</style>
+
+
+
+
